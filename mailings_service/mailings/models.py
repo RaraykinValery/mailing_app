4e85b9af03a647c96d1c85ff9f378c77fd5596b9
Valery Raraykin <vand_er@mail.ru>
2024-01-14 21:37:54 +0300

    Created models

---
+++ mailings_service/mailings/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+from django.core.validators import RegexValidator
+import pytz
+
+
+TIMEZONES = tuple(zip(pytz.all_timezones, pytz.all_timezones))
+
+
+class Mailing(models.Model):
+    start_date = models.DateTimeField("Дата начала рассылки")
+    stop_date = models.DateTimeField("Дата окончания рассылки")
+    text = models.CharField("Текст сообщения", max_length=255)
+    filter = models.CharField("Фильтр свойств клиентов", max_length=255)
+
+
+class Client(models.Model):
+    phone_regex = RegexValidator(
+        regex=r"^7\d{10}$",
+        message="Phone number must be entered in the format: '79999999999'",
+    )
+    phone_number = models.CharField(
+        "Номер телефона",
+        validators=[phone_regex],
+        max_length=11
+    )
+    operator_code = models.CharField(
+        "Код оператора",
+        max_length=3,
+        editable=False)
+    tag = models.CharField(
+        "Тэг",
+        max_length=255,
+        blank=True)
+    timezone = models.CharField(
+        "Часовой пояс",
+        max_length=32,
+        choices=TIMEZONES,
+        default="UTS"
+    )
+
+
+class Message(models.Model):
+    creation_date = models.DateTimeField(
+        "Дата и время создания сообщения",
+        editable=False
+    )
+    status = models.IntegerField("Статус отправки")
+    mailing = models.ForeignKey(
+        Mailing,
+        on_delete=models.CASCADE,
+        blank=False
+    )
+    client = models.ForeignKey(
+        Client,
+        on_delete=models.CASCADE,
+        blank=False
+    )
