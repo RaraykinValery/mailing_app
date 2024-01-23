f342661f5f0c4f849a9801d00c6aa5fb7a129173
Valery Raraykin <vand_er@mail.ru>
2024-01-23 06:27:46 +0300

    Added pagination

--- mailings_service/mailings_service/settings.py
+++ mailings_service/mailings_service/settings.py
@@ -140,3 +140,8 @@ CELERY_RESULT_BACKEND = os.environ.get(
 
 SENDER_HOST = "127.0.0.1"
 SENDER_PORT = "3000"
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
