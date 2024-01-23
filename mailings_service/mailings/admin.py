91bd81bd65dfd07c22c31d7aa092a4a77413a6c9
Valery Raraykin <vand_er@mail.ru>
2024-01-23 03:08:02 +0300

    Registered models for admin panel

--- mailings_service/mailings/admin.py
+++ mailings_service/mailings/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from mailings.models import Mailing, Client, Message
+
+admin.site.register(Mailing)
+admin.site.register(Client)
+admin.site.register(Message)
